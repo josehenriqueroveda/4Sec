00f4077635427359827be315a2801acccce44ebf
José Henrique Roveda <46685746+josehenriqueroveda@users.noreply.github.com>
2024-01-03 14:26:20 -0300

    Update screen_locker.py

--- screen-locker/screen_locker.py
+++ screen-locker/screen_locker.py
@@ -6,10 +6,13 @@ BLOCKED_AREA = (0, 230, 65, 1270)  # (x1, y1, x2, y2)
 SAFE_POSITION = (screen_width / 2, screen_height / 2)
 
 while True:
-    x, y = pyautogui.position()
-    if (
-        BLOCKED_AREA[0] <= x <= BLOCKED_AREA[2]
-        and BLOCKED_AREA[1] <= y <= BLOCKED_AREA[3]
-    ):
-        pyautogui.moveTo(SAFE_POSITION)
-    time.sleep(0.01)
+    try:
+        x, y = pyautogui.position()
+        if (
+            BLOCKED_AREA[0] <= x <= BLOCKED_AREA[2]
+            and BLOCKED_AREA[1] <= y <= BLOCKED_AREA[3]
+        ):
+            pyautogui.moveTo(SAFE_POSITION)
+        time.sleep(0.01)
+    except Exception:
+        continue
