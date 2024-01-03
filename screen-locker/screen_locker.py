a7fbd0ddf4dddd2b0f49cf5421d91e3aa5949e72
José Henrique Roveda <46685746+josehenriqueroveda@users.noreply.github.com>
2024-01-03 14:32:27 -0300

    Refactor screen_locker.py

--- screen-locker/screen_locker.py
+++ screen-locker/screen_locker.py
@@ -1,18 +1,31 @@
 import pyautogui
 import time
 
-screen_width, screen_height = pyautogui.size()
-BLOCKED_AREA = (0, 230, 65, 1270)  # (x1, y1, x2, y2)
-SAFE_POSITION = (screen_width / 2, screen_height / 2)
+def get_screen_size():
+    return pyautogui.size()
 
-while True:
-    try:
-        x, y = pyautogui.position()
-        if (
-            BLOCKED_AREA[0] <= x <= BLOCKED_AREA[2]
-            and BLOCKED_AREA[1] <= y <= BLOCKED_AREA[3]
-        ):
-            pyautogui.moveTo(SAFE_POSITION)
-        time.sleep(0.01)
-    except Exception:
-        continue
+def is_position_blocked(x, y, blocked_area):
+    return (
+        blocked_area[0] <= x <= blocked_area[2]
+        and blocked_area[1] <= y <= blocked_area[3]
+    )
+
+def move_to_safe_position(safe_position):
+    pyautogui.moveTo(safe_position)
+
+def main():
+    screen_width, screen_height = get_screen_size()
+    BLOCKED_AREA = (0, 230, 65, 1270)  # (x1, y1, x2, y2)F
+    SAFE_POSITION = (screen_width / 2, screen_height / 2)
+
+    while True:
+        try:
+            x, y = pyautogui.position()
+            if is_position_blocked(x, y, BLOCKED_AREA):
+                move_to_safe_position(SAFE_POSITION)
+            time.sleep(0.01)
+        except Exception:
+            continue
+
+if __name__ == "__main__":
+    main()
