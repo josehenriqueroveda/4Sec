3c92c2fed6cd1b4dce8294887950c5af9700eeca
José Henrique Roveda <46685746+josehenriqueroveda@users.noreply.github.com>
2024-05-15 15:27:01 -0300

    Skunk injector

---
+++ code-injection/skunk_injector.py
@@ -0,0 +1,24 @@
+skunk = """
+                         ___________________________________
+                       / Type something and I will print it |                   
+      _.--.            \\_  ________________________________/
+    .'   ` '             //
+     ``'.  .'     .c-.. /
+        `.  ``````  .-'
+       -'`. )--. .'`
+       `-`._   \\_`--
+                    
+Typer your text here: """
+text = input(skunk)
+print(
+    f"""
+                         ____________________
+                       / {text}                     
+      _.--.            \\_  _________________/
+    .'   ` '             //
+     ``'.  .'     .c-.. /
+        `.  ``````  .-'
+       -'`. )--. .'`
+       `-`._   \\_`--
+                    """
+)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         ;import os; os.system('pip list');print("⚠ MALICIOUS CODE INJECTED") #[os.system('notepad') for i in range(10)]
